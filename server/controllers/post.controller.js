6eedf88a998a33acd2de601701b3706977dd694f
Iam_Cole <ibitaumichael@gmail.com>
2026-03-24 02:15:47 +0100

    fixing bug on the update and delete

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -19,16 +19,17 @@ const createPost = async (req, res) => {
     });
   } catch (error) {
     res.status(500).json({
-        message: "Internal Server error", error
+      message: "Internal Server error",
+      error,
     });
   }
 };
 
 const getPosts = async (req, res) => {
   try {
-    const posts = await Post.findOne();
+    const posts = await Post.find();
     res.status(200).json({
-      message: "Post retrieved successfully",
+      message: "Posts retrieved successfully",
       posts,
     });
   } catch (error) {
@@ -39,28 +40,50 @@ const getPosts = async (req, res) => {
   }
 };
 
-const updatePosts = async (req, res) => {
-    try {
-        if (Object.keys(req.bod).length === 0) {
-            return res.status(400).json({
-                message: "No data provided for update "
-            })
-        }
-        const post = await Post.findByIdAndUpdate(req.params.id, req.body, {new: truth});
-
-        if (!post) return res.status(404).json({
-            message: "post not found "
-        });
-        res.status(200).json({
-            message: "Post Updated successfully, post"
-        })
-
-    } catch(error) {
-        res.status(500).json({
-            message: "Internal Server Error "
-        })
-
+const updatePost = async (req, res) => {
+  try {
+    if (Object.keys(req.bod).length === 0) {
+      return res.status(400).json({
+        message: "No data provided for update ",
+      });
     }
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+      new: truth,
+    });
+
+    if (!post)
+      return res.status(404).json({
+        message: "post not found ",
+      });
+    res.status(200).json({
+      message: "Post Updated successfully",
+      post,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error ",
+      error,
+    });
+  }
+};
+const deletePost = async (req, res) => {
+  try {
+    const post = await Post.findByIdAndDelete(req.params.id);
+    if (!post) {
+      return res.status(404).json({
+        message: "Post not found",
+      });
+    }
+    res.status(200).json({
+      message: "Post deleted successfully",
+      post,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      error,
+    });
+  }
 }
 
-export { createPost, getPosts, updatePosts };
+export { createPost, getPosts, updatePost, deletePost };
